27109ed80f4fbd088a484fbe3d5bc283b7bf3b42
Tom <noobworldorder2017@gmail.com>
2017-05-23 22:23:22 -0400

    finished profanity filter

--- profanity_filter.py
+++ profanity_filter.py
@@ -1,7 +1,17 @@
+import urllib.request
+
 def read_from_file():
     file_to_open = open('test_document.txt')
     content = file_to_open.read()
-    print(content)
+    full_formated_string = str(content).replace('\n', ' ').replace(' ', '%20')
     file_to_open.close()
+    check_profanity(full_formated_string)
+
+
+def check_profanity(text_to_check):
+    url = r"http://www.purgomalum.com/service/xml?text={}".format(text_to_check)
+    request = urllib.request.Request(url)
+    response = urllib.request.urlopen(request)
+    print(response.read().decode('utf-8'))
 
 read_from_file()
